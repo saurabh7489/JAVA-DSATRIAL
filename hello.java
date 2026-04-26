43f01b8f77520c58ff33a0b83cfaf1c8d0ed5a1e
saurabh7489 <saurabhpandey7489@gmail.com>
2026-04-27 00:07:17 +0530

    initial commit

--- hello.java
+++ hello.java
@@ -1,8 +1,15 @@
 public class hello{
 public static void main(String[] args) {
-  System.out.println("*");
-   System.out.println("**");
-   System.out.println("***");
-   System.out.println("****");
+//   System.out.println("*");
+//    System.out.println("**");
+//    System.out.println("***");
+//    System.out.println("****");
+//veriable
+int a=20;
+int b=10;
+int sum=a+b;
+int min=a-b;
+System.out.println(sum);
+System.out.println(min);
 }
 }
