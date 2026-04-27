5401dd4e09f7c2990147afaff76fdf784f995a59
saurabh7489 <saurabhpandey7489@gmail.com>
2026-04-27 23:35:20 +0530

    initial commit

--- hello.java
+++ hello.java
@@ -1,3 +1,4 @@
+import java.util.*;
 public class hello{
 public static void main(String[] args) {
 //   System.out.println("*");
@@ -5,11 +6,41 @@ public static void main(String[] args) {
 //    System.out.println("***");
 //    System.out.println("****");
 //veriable
-int a=20;
-int b=10;
-int sum=a+b;
-int min=a-b;
-System.out.println(sum);
-System.out.println(min);
+// int a=20;
+// int b=10;
+// int sum=a+b;
+// int min=a-b;
+// System.out.println(sum);
+// System.out.println(min);
+
+// for(int i =1;i<=4;i++){
+//     for(int j=1;j<=i;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println("^");
+// }
+// for(int i=1;i<=3;i++){
+//     for(int j=i;j<=3;j++){
+//         System.out.print("*");
+//     }
+//     System.out.println();
+// }
+// }
+// }
+//    Scanner sc=new Scanner(System.in);
+//    System.out.println("enter the number a");
+//    int a= sc.nextInt();
+//    System.out.println("enter the number b");
+//    int b= sc.nextInt();
+//    int sum=a+b;
+//    System.out.println("this is the sum of your number and b ");
+//    System.out.println(sum);
+//     }
+// }
+for(int i=1;i<=4;i++){
+    for(int j=1;j<=5;j++){
+        System.out.print("*");
+    }
+    System.out.println();
 }
-}
+}}
