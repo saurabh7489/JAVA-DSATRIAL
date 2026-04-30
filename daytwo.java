d4551ebdbc571c6276c5363e694a4955c6e8e780
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-01 00:00:48 +0530

    initial commit

---
+++ daytwo.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class daytwo {
+
+    public static void main(String[] args) {
+        int n=3;
+        int mark[]=new int[n];
+        mark[0]=96;
+        mark[1]=89;
+        mark[2]=99;
+        for(int i=0;i<=n;i++){
+            System.out.println(mark[i]);
+        }
+    }
+}
