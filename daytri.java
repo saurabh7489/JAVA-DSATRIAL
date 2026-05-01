197116bdab711429012f1e1c2dee16e391c0d61c
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-01 22:39:56 +0530

    initial commit

---
+++ daytri.java
@@ -0,0 +1,77 @@
+// linear search
+
+// import java.util.*;
+// public class daytri {
+
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int size=  sc.nextInt();
+//         int arr[]=new int[size];
+//         int x=3;
+//          for(int i=0;i<size;i++){
+//         arr[i]= sc.nextInt();
+//          }
+//          for(int i=0;i<size;i++){
+//             if(x == arr[i]){
+// System.out.println("the valu at index:- "+ i);
+//             }
+            
+//          }
+        
+//     }
+// }
+
+// 2D arrays 
+
+// import java.util.*;
+// public class daytri {
+
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int r =sc.nextInt();
+//         int c=sc.nextInt();
+//         int num[][]= new int[r][c];
+
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 num[i][j]=sc.nextInt();
+//             }
+//         }
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 System.out.print(num[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+import java.util.*;
+public class daytri {
+
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        int r = sc.nextInt();
+        int c =sc.nextInt();
+        System.out.println("enter nth value to find");
+        int n = sc.nextInt();
+
+        int num[][]= new int[r][c];
+
+        for(int i=0;i<r;i++){
+            for(int j=0;j<c;j++){
+                num[i][j]= sc.nextInt(); //input of matrix
+            }
+        }
+        for(int i=0;i<r;i++){  //
+            for(int j=0;j<c;j++){
+                if( n==num[i][j]){ // compare the nth value with matrix
+                    System.out.print("the value is in index (" + i +","+ j + ")");
+                    
+                }
+                
+            }
+           
+        }
+    }
+}
