ab3b1a28672219d9fab703a8cc303a2808917f34
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-01 23:58:04 +0530

    initial commit

--- daytri.java
+++ daytri.java
@@ -46,32 +46,54 @@
 //     }
 // }
 
+// import java.util.*;
+// public class daytri {
+
+//     public static void main(String[] args) {
+//         Scanner sc =new Scanner(System.in);
+//         int r = sc.nextInt();
+//         int c =sc.nextInt();
+//         System.out.println("enter nth value to find");
+//         int n = sc.nextInt();
+
+//         int num[][]= new int[r][c];
+
+//         for(int i=0;i<r;i++){
+//             for(int j=0;j<c;j++){
+//                 num[i][j]= sc.nextInt(); //input of matrix
+//             }
+//         }
+//         for(int i=0;i<r;i++){  //
+//             for(int j=0;j<c;j++){
+//                 if( n==num[i][j]){ // compare the nth value with matrix
+//                     System.out.print("the value is in index (" + i +","+ j + ")");
+                    
+//                 }
+                
+//             }
+           
+//         }
+//     }
+// }
+
 import java.util.*;
 public class daytri {
 
     public static void main(String[] args) {
-        Scanner sc =new Scanner(System.in);
-        int r = sc.nextInt();
-        int c =sc.nextInt();
-        System.out.println("enter nth value to find");
-        int n = sc.nextInt();
-
-        int num[][]= new int[r][c];
-
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                num[i][j]= sc.nextInt(); //input of matrix
-            }
+        Scanner sc= new Scanner(System.in);
+        int size= sc.nextInt();
+        int target= sc.nextInt();
+        int Array[] = new int[size];
+        for(int i=0;i<size;i++){
+            Array[i] = sc.nextInt();
         }
-        for(int i=0;i<r;i++){  //
-            for(int j=0;j<c;j++){
-                if( n==num[i][j]){ // compare the nth value with matrix
-                    System.out.print("the value is in index (" + i +","+ j + ")");
-                    
-                }
-                
-            }
-           
+   for(int i=0;i<size;i++){
+    for(int j=1;j<size;j++){
+        if(target==Array[i]+Array[j]){
+System.out.println("["+i+","+j+"]");
         }
+    }
+   }
+
     }
 }
