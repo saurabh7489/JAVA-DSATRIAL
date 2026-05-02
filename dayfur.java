6afcd316c4798009e3cd372848e0aa56f41e668c
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-02 23:48:15 +0530

    initial commit

--- dayfur.java
+++ dayfur.java
@@ -1,7 +1,16 @@
+// import java.util.*;
+// public class dayfur {
+//     public static void main(String[] args) {
+//         String name="saurabh";
+//         System.out.println(name);
+//     }
+// }
+
 import java.util.*;
 public class dayfur {
     public static void main(String[] args) {
-        String name="saurabh";
+        StringBuilder name=new StringBuilder("sourabh");
+        name.setCharAt(0,'S');
         System.out.println(name);
     }
 }
