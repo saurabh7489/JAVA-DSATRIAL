f6ef324a14074880174cf933b5b7cea1bca39562
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-03 23:22:20 +0530

    initial commit

--- dayfur.java
+++ dayfur.java
@@ -6,11 +6,32 @@
 //     }
 // }
 
+// import java.util.*;
+// public class dayfur {
+//     public static void main(String[] args) {
+//         StringBuilder name=new StringBuilder("sourabh");
+//         name.setCharAt(0,'S');
+//         System.out.println(name);
+//   
+
 import java.util.*;
 public class dayfur {
+
+    public static boolean isPalindrome(int x) {
+     
+int original = x;
+int palin = 0;
+
+while(x > 0){
+    int rem = x % 10;
+    palin = palin * 10 + rem;
+    x = x / 10;
+}
+
+return palin == original;
+    
+}
     public static void main(String[] args) {
-        StringBuilder name=new StringBuilder("sourabh");
-        name.setCharAt(0,'S');
-        System.out.println(name);
+        System.out.println(isPalindrome(121));
     }
 }
