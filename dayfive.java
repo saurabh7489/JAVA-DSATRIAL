d68558a53dc4c66d01dfe950137917bf10c88f00
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-04 22:56:20 +0530

    initial commit

--- dayfive.java
+++ dayfive.java
@@ -1,23 +1,23 @@
-import java.util.*;
+// import java.util.*;
 
-public class dayfive {
-    public static void main(String[] args) {
-        int arr[] = {7, 8, 3, 1, 2};
+// public class dayfive {
+//     public static void main(String[] args) {
+//         int arr[] = {7, 8, 3, 1, 2};
         
-        for(int i = 0; i < arr.length - 1; i++) {
-            for(int j = 0; j < arr.length - i - 1; j++) {
-                if(arr[j] > arr[j + 1]) {
-                    // swap
-                    int temp = arr[j];
-                    arr[j] = arr[j + 1];
-                    arr[j + 1] = temp;
-                }
-            }
-        }
+//         for(int i = 0; i < arr.length - 1; i++) {
+//             for(int j = 0; j < arr.length - i - 1; j++) {
+//                 if(arr[j] > arr[j + 1]) {
+//                     // swap
+//                     int temp = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+//                 }
+//             }
+//         }
 
-        System.out.println("Sorted Array:");
-        for(int num : arr) {
-            System.out.print(num + " ");
-        }
-    }
-}
+//         System.out.println("Sorted Array:");
+//         for(int num : arr) {
+//             System.out.print(num + " ");
+//         }
+//     }
+// }
