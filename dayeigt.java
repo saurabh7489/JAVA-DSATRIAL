f0e15c6715ff84583fb234900ebd25abde72d32d
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-07 22:20:30 +0530

    initial commit

--- dayeigt.java
+++ dayeigt.java
@@ -11,17 +11,19 @@
 //        }
 // System.out.print(count+"");
 
-import java.util.*;
-public class dayeigt {
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-       int n= sc.nextInt();
-        int collect=0;
-        while(n>0){
-            int lastDigit=n%10;
-            n=n/10;
-            collect=lastDigit+collect*10;
-        }
-        System.out.println(collect); 
-    }
-}
+// reverse the num
+
+// import java.util.*;
+// public class dayeigt {
+//     public static void main(String[] args) {
+//         Scanner sc=new Scanner(System.in);
+//        int n= sc.nextInt();
+//         int collect=0;
+//         while(n>0){
+//             int lastDigit=n%10;
+//             n=n/10;
+//             collect=lastDigit+collect*10;
+//         }
+//         System.out.println(collect); 
+//     }
+// }
