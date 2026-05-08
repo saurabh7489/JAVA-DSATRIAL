78cd24f0bd30e645bb72ddf4435f6b9022a00d4e
saurabh7489 <saurabhpandey7489@gmail.com>
2026-05-08 23:38:57 +0530

    initial commit

--- daytn.java
+++ daytn.java
@@ -34,23 +34,37 @@
 //     }
 // }
 
+// import java.util.*;
+// public class daytn {
+
+//     public static void main(String[] args) {
+//         int n=6;
+//         int count=0;
+//         for(int i=1;i<=n;i++){
+//             if(n%i==0){
+//                 count=count+i;
+//             }
+
+//         }
+//         if(count==n+1){
+//             System.err.println("Number is prime");
+//         }
+//         else{
+//             System.out.println("number is not prime");
+//         }
+//     }
+// }
+
 import java.util.*;
 public class daytn {
 
     public static void main(String[] args) {
-        int n=6;
-        int count=0;
+        int n=2;
+        int n1=12;
         for(int i=1;i<=n;i++){
-            if(n%i==0){
-                count=count+i;
+            if(n%i==0 && n1%i==0){
+                System.out.println(i);
             }
-
-        }
-        if(count==n+1){
-            System.err.println("Number is prime");
-        }
-        else{
-            System.out.println("number is not prime");
         }
     }
 }
